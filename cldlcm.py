69b8bc43713056c887a9ef2c374ec3361eb3657d
Todinhthuc <todinhthuc08031986@gmail.com>
2025-07-10 11:55:39 +0700

    commit

---
+++ cldlcm.py
@@ -0,0 +1,7 @@
+n= int(input("Enter a number: ")) 
+m= int(input("Enter a number: ")) 
+for i in range(0, 1,1):
+    if m % n == 0:
+        print("chia het")
+    if m % n != 0:
+        print("ko chia het")
