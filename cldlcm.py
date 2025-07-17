fe633c52aff1822870070e6ffc9ba2f3580d4aae
Todinhthuc <todinhthuc08031986@gmail.com>
2025-07-17 09:59:42 +0700

    first commit

--- cldlcm.py
+++ cldlcm.py
@@ -1,7 +1,39 @@
-n= int(input("Enter a number: ")) 
-m= int(input("Enter a number: ")) 
-for i in range(0, 1,1):
-    if m % n == 0:
-        print("chia het")
-    if m % n != 0:
-        print("ko chia het")
+print("task 1")
+a=int(input("Enter a number: "))
+if a>0:
+    print("Positive")
+if a<0:
+    print("Negative")
+if a==0:
+    print("Zero")
+print("task 2")
+height= int(input("Enter a number(cm): "))
+if height>120:
+    print("price= 50$")
+if height<=120:
+    print("price= free")
+print("task 3")
+age= int(input("Enter a number: "))
+height== int(input("Enter a number(cm): "))
+if age>15:
+    print("price= 50$")
+if height>120:
+    print("price= 25$")
+else:
+    print("price= free")
+print("task 4")
+Febuary= (29)
+April=June=September=November=(30) 
+January=March=May=July=August=October=December= (31)
+print(January+Febuary+March+April+May+June+July+August+November+October+November+December)
+print("task 5")
+a=int(input("Enter a number: "))
+if a%2 ==0:
+    print("a divided by 2")
+if a%2 !=0:
+    print("a divided 2 remain")
+if a%3 ==0:
+    print("a divided by 3")
+if a%3 !=0:
+    print("a divided 3 remain")
+# check answer
